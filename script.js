97ce4ea996712a88f9d76741adeb7a75abb1408a
dhruvpurohit2k <purohitdhruv00@gmail.com>
2024-07-25 13:47:20 +0530

    Make Library class and refactor the functionality of adding a book to the shelf

--- script.js
+++ script.js
@@ -1,20 +1,51 @@
+class Library {
+  constructor(bookShelfElement) {
+    this.bookShelfElement = bookShelfElement;
+    console.log(this.bookShelfElement);
+  }
+
+  addBook(bookName, authorName, pages) {
+    let div = document.createElement("div");
+    div.classList.add("bookcard");
+    div.innerHTML = `<p>${bookName.value}</p>
+      <p>${authorName.value}</p>
+      <p>${pages.value}</p>
+      <div class="read-checkbox">
+        <label>Read</label>
+        <input type="checkbox" name="read">
+      </div>
+      <div class="remove-book">
+        <img src="/svg/close.svg" alt="">
+      </div>`;
+    // document.querySelector("#bookshelf").appendChild(div);
+    this.bookShelfElement.appendChild(div);
+    bookName.value = "";
+    authorName.value = "";
+    pages.value = "";
+  }
+
+}
 let modal = document.querySelector("dialog");
 let cardlist = document.querySelectorAll(".bookcard");
-
+let lib = new Library(document.querySelector("#bookshelf"));
 let addBook = document.querySelector("#addbook");
 addBook.addEventListener("click", () => {
   modal.showModal();
 })
 
 modal.addEventListener("submit", (e) => {
-  createBookCard();
-  cardlist = document.querySelectorAll(".bookcard");
-  console.log(cardlist);
-  cardlist.forEach((card) => {
-    card.querySelector(".remove-book").addEventListener("click", () => {
-      card.remove();
-    })
-  })
+  let bookname = modal.querySelector("#bookname");
+  let authorname = modal.querySelector("#authorname");
+  let pages = modal.querySelector("#pages");
+  lib.addBook(bookname, authorname, pages);
+  // createBookCard();
+  // cardlist = document.querySelectorAll(".bookcard");
+  // console.log(cardlist);
+  // cardlist.forEach((card) => {
+  //   card.querySelector(".remove-book").addEventListener("click", () => {
+  //     card.remove();
+  //   })
+  // })
   modal.close();
 })
 
@@ -26,21 +57,5 @@ function createBookCard() {
   let bookname = modal.querySelector("#bookname");
   let authorname = modal.querySelector("#authorname");
   let pages = modal.querySelector("#pages");
-  let div = document.createElement("div");
-  div.classList.add("bookcard");
-  div.innerHTML = `<p>${bookname.value}</p>
-      <p>${authorname.value}</p>
-      <p>${pages.value}</p>
-      <div class="read-checkbox">
-        <label>Read</label>
-        <input type="checkbox" name="read">
-      </div>
-      <div class="remove-book">
-        <img src="/svg/close.svg" alt="">
-      </div>`;
-  document.querySelector("#bookshelf").appendChild(div);
-  bookname.value = "";
-  authorname.value = "";
-  pages.value = "";
-
 }
+
